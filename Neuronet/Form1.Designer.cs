96f1924e2e6e8f76631b102895fccdae1fb2d210
Pansun <chkehua@gmail.com>
2020-12-24 19:23:41 +0700

    添加项目文件。

---
+++ Neuronet/Form1.Designer.cs
@@ -0,0 +1,311 @@
+﻿namespace Neuronet
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.textBox8 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(443, 459);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 39);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Выход";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(308, 16);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(194, 54);
+            this.button2.TabIndex = 0;
+            this.button2.Text = "Обучить";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(14, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(126, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Коэффициент обучения";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(146, 16);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(14, 53);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(67, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Кол-во эпох";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(87, 50);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 2;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(192, 85);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(14, 88);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(172, 13);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Размер тренировочной выборки";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(23, 402);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(146, 13);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "Результаты тестирования: ";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(192, 115);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 20);
+            this.textBox4.TabIndex = 2;
+            // 
+            // chart1
+            // 
+            chartArea2.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea2);
+            this.chart1.Location = new System.Drawing.Point(16, 153);
+            this.chart1.Name = "chart1";
+            series2.BorderWidth = 2;
+            series2.ChartArea = "ChartArea1";
+            series2.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series2.Color = System.Drawing.Color.Blue;
+            series2.Name = "Error";
+            this.chart1.Series.Add(series2);
+            this.chart1.Size = new System.Drawing.Size(501, 236);
+            this.chart1.TabIndex = 3;
+            this.chart1.Text = "chart1";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(40, 118);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(142, 13);
+            this.label5.TabIndex = 1;
+            this.label5.Text = "Размер тестовой выборки";
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(359, 111);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(100, 20);
+            this.textBox5.TabIndex = 2;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(345, 88);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(122, 13);
+            this.label6.TabIndex = 1;
+            this.label6.Text = "Погрешность решения";
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(52, 438);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(83, 20);
+            this.textBox6.TabIndex = 2;
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(158, 438);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(83, 20);
+            this.textBox7.TabIndex = 2;
+            // 
+            // textBox8
+            // 
+            this.textBox8.Location = new System.Drawing.Point(264, 438);
+            this.textBox8.Name = "textBox8";
+            this.textBox8.Size = new System.Drawing.Size(83, 20);
+            this.textBox8.TabIndex = 2;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(250, 467);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(99, 31);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "предсказать";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(30, 441);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(19, 13);
+            this.label7.TabIndex = 1;
+            this.label7.Text = "a: ";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(141, 441);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(19, 13);
+            this.label8.TabIndex = 1;
+            this.label8.Text = "b: ";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(247, 441);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(19, 13);
+            this.label9.TabIndex = 1;
+            this.label9.Text = "c: ";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(16, 476);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(65, 13);
+            this.label10.TabIndex = 1;
+            this.label10.Text = "Результат: ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(530, 514);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.textBox8);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Умножение трех чисел";
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.TextBox textBox8;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+    }
+}
+
---
+++ Neuronet/Form1.cs
@@ -0,0 +1,343 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Neuronet
+{
+    public partial class Form1 : Form
+    {
+        static Random rd; //генератор рандомных чисел
+        static double[,] w1; //веса между входом и скрытым слоем
+        static double[,] w2; //веса между скрытым слоем и выходом
+        static double[] b1; //веса смещения для скрытого слоя
+        static double[] b2; //веса смещения для выходного слоя
+        static double l_rate; //коэффициент обучения
+
+        static int epoch; //кол-во эпох обучения
+        static int train_size; //кол-во тренировочных данных
+        static int test_size; //кол-во данных для тестирования
+
+        //структура нейронной сети
+        static int inp; //кол-во нейронов на входе
+        static int hid; //кол-во нейронов на скрытом слое
+        static int outs; //кол-во нейронов на выходе
+
+        //промежуточные значения
+        static double[] z_in; //выход на скрытом слое без функции активации
+        static double[] z; //с функцией активации
+        static double[] y_in; //выход сети без функции активации
+        static double[] y; //с ней
+
+        //значения для корректировки весов сети
+        static double[] qy;
+        static double[] qz;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            textBox1.Text = "0.1";
+            textBox2.Text = "100";
+            textBox3.Text = "500";
+            textBox4.Text = "100";
+            textBox5.Text = "0.01";
+            textBox6.Text = "0.1";
+            textBox7.Text = "0.5";
+            textBox8.Text = "0.65";
+
+            inp = 3;
+            hid = 10;
+            outs = 1;
+
+            train_size = 500;
+            test_size = 100;
+
+            rd = new Random();
+
+            //инициализация весов сети
+            w1 = new double[hid, inp];
+            w2 = new double[outs, hid];
+
+            //инициализация весов смещения
+            b1 = new double[hid];
+            b2 = new double[outs];
+
+            Randomize(); //заполняем их случайными значениями
+
+            //инициализация побочных значений
+            z_in = new double[hid];
+            z = new double[hid];
+            y_in = new double[outs];
+            y = new double[outs];
+
+            //инициализация значений для корректировки всех весов сети
+            qy = new double[outs];
+            qz = new double[hid];
+        }
+
+        //функция активации (сигмоида)
+        private static double f(double x)
+        {
+            return 1.0 / (1.0 + Math.Exp(-x));
+        }
+
+        //производная функции активации
+        private static double df(double x)
+        {
+            return f(x) * (1.0 - f(x));
+        }
+
+        //прямое распространение сигнала
+        private static void ForwardPropagate(double[] x)
+        {
+            //скрытый слой
+            for (int i = 0; i < hid; i++)
+            {
+                double s = 0;
+                for (int j = 0; j < inp; j++)
+                {
+                    s += w1[i, j] * x[j];
+                }
+                z_in[i] = s + b1[i];
+                z[i] = f(z_in[i]);
+            }
+
+            //выход
+            for (int i = 0; i < outs; i++)
+            {
+                double s = 0;
+                for (int j = 0; j < hid; j++)
+                {
+                    s += w2[i, j] * z[j];
+                }
+                y_in[i] = s + b2[i];
+                y[i] = f(y_in[i]);
+            }
+        }
+
+        //обратный ход
+        private static void BackwardPropagate(double[] t)
+        {
+            //считаем ошибку на выходе
+            for (int i = 0; i < outs; i++)
+            {
+                qy[i] = (t[i] - y[i]) * df(y_in[i]);
+            }
+
+            //считаем ошибку на скрытом слое
+            for (int i = 0; i < hid; i++)
+            {
+                double s = 0;
+                for (int j = 0; j < outs; j++)
+                {
+                    s += w2[j, i] * qy[j];
+                }
+                qz[i] = s * df(z_in[i]);
+            }
+        }
+
+        //обновление всех весов сети
+        private static void UpdateWeights(double[] x)
+        {
+            //веса между скрытым слоем и выходом
+            for (int i = 0; i < outs; i++)
+            {
+                for (int j = 0; j < hid; j++)
+                {
+                    w2[i, j] += l_rate * qy[i] * z[j];
+                }
+            }
+
+            //веса между входом и скрытым слоем
+            for (int i = 0; i < hid; i++)
+            {
+                for (int j = 0; j < inp; j++)
+                {
+                    w1[i, j] += l_rate * qz[i] * x[j];
+                }
+            }
+
+            //веса смещения на выходе
+            for (int i = 0; i < outs; i++)
+            {
+                b2[i] += l_rate * qy[i];
+            }
+
+            //веса смещения на скрытом слое
+            for (int i = 0; i < hid; i++)
+            {
+                b1[i] += l_rate * qz[i];
+            }
+        }
+
+        //выход из программы
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        //для получения строки из двумерного массива
+        private static double[] row(double[,] x,int idx)
+        {
+            double[] res = new double[x.GetLength(1)];
+            for(int i = 0; i < 3; i++)
+            {
+                res[i] = x[idx,i];
+            }
+            return res;
+        }
+
+        //среднеквадратическая ошибка
+        private static double MSE(double[] y1, double[] y2)
+        {
+            double error = 0;
+            for(int i = 0; i < y1.Length; i++)
+            {
+                error += Math.Pow(y1[i] - y2[i], 2);
+            }
+            return error / y1.Length;
+        }
+
+        //обучение нейронной сети посредством обратного распространения
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //сброс весов сети
+            Randomize();
+
+            //получаем все необходимые значения с формы
+            l_rate = GetContolText(textBox1);
+            epoch = int.Parse(textBox2.Text);
+            train_size = int.Parse(textBox3.Text);
+            test_size = int.Parse(textBox4.Text);
+
+            chart1.Series["Error"].Points.Clear();
+            chart1.ChartAreas[0].AxisX.Title = "Epoch";
+            chart1.ChartAreas[0].AxisY.Title = "Error";
+
+            //создаем случайным образом 3 числа из диапазона [0..1]
+            double[,] train_x = new double[train_size,3];
+            double[] train_y = new double[train_size];
+            double[,] test_x = new double[test_size,3];
+            double[] test_y = new double[test_size];
+            double[] y_pred = new double[train_size];
+
+            //для тренировки
+            for (int i = 0; i < train_size; i++)
+            {
+                double a = rd.NextDouble();
+                double b = rd.NextDouble();
+                double c = rd.NextDouble();
+                train_x[i,0] = a;
+                train_x[i, 1] = b;
+                train_x[i, 2] = c;
+                train_y[i] = a * b * c;
+            }
+
+            //для теста
+            for (int i = 0; i < test_size; i++)
+            {
+                double a = rd.NextDouble();
+                double b = rd.NextDouble();
+                double c = rd.NextDouble();
+                test_x[i, 0] = a;
+                test_x[i, 1] = b;
+                test_x[i, 2] = c;
+                test_y[i] = a * b * c;
+            }
+
+            //цикл по эпохам
+            for(int i = 0; i < epoch; i++)
+            {
+                //цикл по каждому элементу тренировочной выборки
+                for(int j = 0; j < train_size; j++)
+                {
+                    ForwardPropagate(row(train_x,j));
+                    y_pred[j] = y[0];
+                    BackwardPropagate(new double[] { train_y[j] });
+                    UpdateWeights(row(train_x, j));
+                }
+
+                chart1.Series["Error"].Points.AddXY(i, MSE(train_y, y_pred));
+            }
+
+            //тестируем обученную сеть
+            double eps = GetContolText(textBox5); //точность найденного решения
+            int tr = 0; //кол-во верных значений
+            int fl = 0; //кол-во неверных
+            y_pred = new double[test_size];
+            for (int i = 0; i < test_size; i++)
+            {
+                ForwardPropagate(row(test_x, i));
+                y_pred[i] = y[0];
+                if(Math.Abs(y_pred[i]-test_y[i]) <= eps)
+                {
+                    tr++;
+                }
+                else
+                {
+                    fl++;
+                }
+            }
+
+            label4.Text = "Результаты тестирования: " + "MSE: " + MSE(test_y, y_pred) + "; Error: " + eps + "\n" +
+                "Правильных значений: " + tr + ";  Неправильных значений: " + fl + ";"; 
+            
+        }
+
+        //сброс весов сети для повторного их обучения
+        private static void Randomize()
+        {
+            for (int i = 0; i < hid; i++)
+            {
+                for (int j = 0; j < inp; j++)
+                {
+                    w1[i, j] = rd.NextDouble();
+                }
+            }
+
+            for (int i = 0; i < outs; i++)
+            {
+                for (int j = 0; j < hid; j++)
+                {
+                    w2[i, j] = rd.NextDouble();
+                }
+            }
+
+            //инициализация весов смещения
+            for (int i = 0; i < hid; i++)
+            {
+                b1[i] = rd.NextDouble();
+            }
+
+            for (int i = 0; i < outs; i++)
+            {
+                b2[i] = rd.NextDouble();
+            }
+        }
+
+        //получаем значения из формы заменяя при этом точку на запятую
+        public double GetContolText(TextBox textBox)
+        {
+            if (textBox.Text != "")
+            {
+                return Convert.ToDouble(textBox.Text.Replace(".", ","));
+            }
+            else
+            {
+                return 0;
+            }
+        }
+
+        //для отдельного тестирования сети на трех заданных числах
+        private void button3_Click(object sender, EventArgs e)
+        {
+            //получаем три значения с формы
+            double a = GetContolText(textBox6);
+            double b = GetContolText(textBox7);
+            double c = GetContolText(textBox8);
+
+            //просчет сетью
+            ForwardPropagate(new double[] { a, b, c });
+            label10.Text = "Результат: y = a*b*c = " + Math.Round((a * b * c),4) + "; y^ = " + Math.Round(y[0],4) + "; ";
+        }
+    }
+}
